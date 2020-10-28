982539f2342c90a458fe5c62ba7e266cee4ee46c
Adam Simmons <hi@adamjsimmons.com>
2020-10-28 16:18:59 -0500

    Rename Browser as 'Sample 8 - Web Browser'

--- samples/Sample 8 - Web Browser/src/Browser.cpp
+++ samples/Sample 8 - Web Browser/src/Browser.cpp
@@ -11,7 +11,7 @@ Browser::Browser()  {
     
   window_ = Window::Create(app_->main_monitor(), 1024, 768, false, 
     kWindowFlags_Resizable | kWindowFlags_Titled | kWindowFlags_Maximizable);
-  window_->SetTitle("Ultralight Sample - Browser");
+  window_->SetTitle("Ultralight Sample 8 - Web Browser");
   app_->set_window(*window_.get());
     
   // Create the UI
